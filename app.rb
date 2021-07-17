81291bb7ffeaa4504aa94c725ed1b601258317aa
4Pavlo2 <pshkoropad@gmail.com>
2021-07-17 18:52:28 +0100

    db+cont+...+placeholder

--- app.rb
+++ app.rb
@@ -2,7 +2,27 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init
+	@db = SQLite3::Database.new "blog.db"
+	@db.results_as_hash = true # щоб у вигляді хещу дані подавалися
+end
+
+before do
+init
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/forum' do
+	erb :forum #"Hello World"
+  end
+
+  post '/forum' do
+	@content = params[:content]
+
+	erb "You typed: #{@content}"
+  end
