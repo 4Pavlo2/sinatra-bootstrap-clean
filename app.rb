b3ab7d3e19184531251f7e9c629e054fe9e88090
4Pavlo2 <pshkoropad@gmail.com>
2021-07-21 17:55:16 +0100

    validation

--- app.rb
+++ app.rb
@@ -17,12 +17,27 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+configure do
+init
+@db.execute 'CREATE TABLE if not exists Posts
+ (
+	 "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "created_date" DATE,
+   "content" TEXT
+   )'
+end
+
 get '/forum' do
 	erb :forum #"Hello World"
   end
 
   post '/forum' do
 	@content = params[:content]
+
+	if @content.length <= 0
+		@error = 'Type text'
+		return erb :forum
+	end
 
 	erb "You typed: #{@content}"
   end
