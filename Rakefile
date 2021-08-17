c6236a099d150bf7ad870455d2deb43e74668338
4Pavlo2 <pshkoropad@gmail.com>
2021-08-17 06:08:04 +0100

    29bar

---
+++ Rakefile
@@ -0,0 +1,10 @@
+require "./app" #
+require "sinatra/activerecord/rake"
+
+=begin
+namespace :db do
+    task :load_config do
+      require "./app"
+    end
+end
+=end
---
+++ app.rb
@@ -0,0 +1,18 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+
+
+set :database, { adapter: 'sqlite3', database: 'barbers.db' }
+#set :database, {adapter: "sqlite3", database: "foo.sqlite3"}
+#set :database, "sqlite:barbers.db"
+
+class Client < ActiveRecord::Base
+end
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
